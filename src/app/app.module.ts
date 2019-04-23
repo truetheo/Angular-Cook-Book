377cedfa800428581808ad8ae1cde611b24a4131
Teofil Burdach <teofil.burdach@ser-solutions.pl>
2019-04-23 13:23:54 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { ShoppingListEditComponent } from './Header/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './Header/recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Header/recipe/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './Header/recipe/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './Header/header.component';
+import { ShoppingComponent } from './Header/shopping/shopping.component';
+import { RecipiesComponent } from './Header/recipe/recipies.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    ShoppingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
